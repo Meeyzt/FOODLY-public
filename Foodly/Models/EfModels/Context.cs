679226b92385c94ec40b7dc17100e58fad48bcbd
Meeyzt <mehmetbs@hotmail.com.tr>
2020-12-02 12:07:06 +0300

    init project

---
+++ Foodly/Models/EfModels/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Foodly.Models.EfModels
+{
+    public class Context:DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("");
+        }
+    }
+}
